c33aa60faf578992865236dc74af2b15310ccf50
JaronKong <jaronkong@gmail.com>
2020-06-26 13:52:12 -0700

    initial work

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # kaiomeris-website
 Website for KaioMeris
+
+Use these commands
+```
+./node_modules/next/dist/bin/next
+./node_modules/next/dist/bin/next export
+./node_modules/next/dist/bin/next start
+```
---
+++ pages/quantum.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import Head from 'next/head'
+
+function HomePage() {
+  return (
+    <div>
+      <Head>
+        <title>Quantum Protocol</title>
+      </Head>
+      <p>Hello world!</p>
+    </div>
+  )
+}
+
+export default HomePage
