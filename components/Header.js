ee2a065959d32c40aaf0fa65115171a8de1dc7d6
JaronKong <jaronkong@gmail.com>
2020-06-26 15:18:51 -0700

    Header and nav

--- components/Header.js
+++ components/Header.js
@@ -6,6 +6,34 @@ import navButtons from '../config/navButtons'
 const headerStyle = {
   color: "white",
   width: "100%",
+  paddingTop: '16px',
+  marginLeft: '16px',
+  marginRight: '16px',
+};
+
+const headerFlexContainerStyle = {
+  padding: '0',
+  margin: '0',
+  'list-style': 'none',
+  '-ms-box-orient': 'horizontal',
+  display: '-webkit-box',
+  display: '-moz-box',
+  display: '-ms-flexbox',
+  display: '-moz-flex',
+  display: '-webkit-flex',
+  display: 'flex',
+
+  '-webkit-flex-direction': 'row',
+  'flex-direction': 'row',
+  'justify-content': 'space-between',
+};
+
+const headerFlexContainerItemStyle = {
+  'line-height': '50px',
+  color: 'white',
+  'font-weight': 'bold',
+  'font-size': '2em',
+  'text-align': 'center',
 };
 
 const logoStyle = {
@@ -15,10 +43,17 @@ const logoStyle = {
 
 const Header = () => (
   <div className="Header" style={headerStyle}>
-    <Link href="/">
-      <a><img src="/logoLight.png" alt="logo" style={logoStyle}/></a>
-    </Link>
-    <NavBar navButtons={navButtons} />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css" />
+    <ul style={headerFlexContainerStyle}>
+      <li style={headerFlexContainerItemStyle}>
+        <Link href="/">
+          <a><img src="/logoLight.png" alt="logo" style={logoStyle}/></a>
+        </Link>
+      </li>
+      <li style={headerFlexContainerItemStyle}>
+        <NavBar navButtons={navButtons} />
+      </li>
+    </ul>
   </div>
 );
 
--- components/nav/NavBar.js
+++ components/nav/NavBar.js
@@ -1,22 +1,36 @@
-import NavButton from "./NavButton";
+import NavButton from './NavButton';
 
 const navBarStyle = {
-  backgroundColor: "red",
-  color: "white",
-  width: "100%",
-  height: "60px"
+  color: 'white',
+  width: '100%',
+};
+
+const navListStyle = {
+  'list-style-type': 'none',
+  margin: '0',
+  paddingRight: '32px',
+  overflow: 'hidden',
+};
+
+const navListItemStyle = {
+  float: 'right',
+  margin: '0px 16px',
 };
 
 const NavBar = props => (
-  <div className="NavBar" style={navBarStyle}>
-    {props.navButtons.map(button => (
-      <NavButton
-        key={button.path}
-        path={button.path}
-        label={button.label}
-        icon={button.icon}
-      />
-    ))}
+  <div className='NavBar' style={navBarStyle}>
+    <ul style={navListStyle}>
+      {props.navButtons.map((button, i)=> (
+        <li style={navListItemStyle} key={i}>
+          <NavButton
+            key={button.path}
+            path={button.path}
+            label={button.label}
+            icon={button.icon}
+          />
+        </li>
+      ))}
+    </ul>
   </div>
 );
 
